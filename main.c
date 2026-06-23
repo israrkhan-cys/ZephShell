125f4d385e2bcab1d201f5d0ca9a6ce72c678126
israrkhan-cys <7israr.work@gmail.com>
2026-06-23 17:09:48 +0500

    Added : Pipes but for one pipe works only
    if the cammand ahve more than one the pipe is yet no desinged to put the output of 2nd to 3rd

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ void execute(char **args){
     if (x < 0){
         perror("Fork Failed");
     }
-
     if(x == 0){
         if(execvp(args[0],args) == -1){
             perror("Error:");
@@ -25,6 +24,80 @@ void execute(char **args){
     }
 }
 
+int check_for_pipes(char **args, int pip){
+    int i=0;
+    while(args[i]!= NULL){
+        if(strcmp(args[i],"|")==0){
+            pip=i;
+            break;
+        }
+        i++;
+    }
+    return pip;
+}
+
+// int Count_pipes(char **args){
+// 	int count=0, i=0; 
+// 	while(args[i] != NULL){	
+// 	      if(strcmp(args[i],"|")==0){
+// 		      count++;
+// 	   }
+// 	      i++;
+//     }
+// 	return count;
+// }
+
+int Execute_pipe_Cammand(char ** args, int pip){
+    //int count_pip= Count_pipes(args);
+    char **cmd1, **cmd2;
+    cmd1=args;
+    cmd2=&args[pip+1];
+
+    int pipefd[2];
+    if(pipe(pipefd) == -1){
+        perror("pipe");
+       exit(EXIT_FAILURE);
+    }
+    pid_t p1 =fork();
+    if(p1 <0){
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if(p1==0){
+        
+        dup2(pipefd[1],STDOUT_FILENO);
+        close(pipefd[0]);
+        close(pipefd[1]);
+	    if(execvp(cmd1[0],cmd1)==-1){
+            perror("exec");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    pid_t p2 =fork();
+    if(p2 <0){
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if(p2==0){
+        dup2(pipefd[0],STDIN_FILENO);
+        close(pipefd[0]);
+        close(pipefd[1]);
+        execvp(cmd2[0],cmd2);
+
+    }
+
+
+    close(pipefd[0]);
+    close(pipefd[1]);
+    waitpid(p1,NULL,0);
+    waitpid(p2,NULL,0);
+   // free(cmd1); free(cmd2);
+    return 1;
+}
+
+
 void changeDirectory(char **args){
     if(args[1] == NULL){
         perror("Error: No arguments");
@@ -32,6 +105,7 @@ void changeDirectory(char **args){
         perror("Error:");
     }
 }
+
 
 void help(){
     printf("=== ZephShell, A Unix shell ===\n");
@@ -67,28 +141,34 @@ int main(){
     // main shell  loop
     do{
         
-        printf(">>");
+        printf(">>>");
         char * lineInput=NULL;
         ssize_t size=0;
         
         getline(&lineInput,&size, stdin);
+    
         lineInput[strcspn(lineInput,"\n")] = '\0';  //replacing the newline "\n" wiht null 
-        char ** args= split_cammand(lineInput);
+         if(strcmp(lineInput,"\0")==0){ //handling the empty inputss
+            continue;
+        }
 
+        char **args= split_cammand(lineInput);
+        int pip=-1;
+        pip=check_for_pipes(args,pip); // checking if the cammand contain pipe to handle it differently 
 
         if(strcmp(args[0],"cd")==0){
             changeDirectory(args);
-
         }else if(strcmp(args[0],"exit")==0){
             exit(0);
 
         }else if(strcmp(args[0],"help")==0) {
             help();
+        }else if (pip != -1){
+            printf("ipiee");
+            Execute_pipe_Cammand(args,pip);
         }else{
-            execute(args);
+         execute(args);
         }
-
-
 
         free(lineInput);
         free(args);
